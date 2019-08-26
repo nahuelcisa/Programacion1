0b95c94798c2faf3f1388fc772b9ba4494815841
nahuelcisa <nahuelcisa@hotmail.com>
2019-08-26 11:48:16 -0300

    agregue carpeta de menu de opciones

---
+++ menudeopciones/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    char opcion;
+    char seguir = 's';
+
+    do{
+
+     system("cls");
+     printf("a- Sumar\n");
+     printf("b- Restar\n");
+     printf("c- Multiplicar\n");
+     printf("d- Dividir\n");
+     printf("e- Salir\n");
+     printf("Ingrese opcion: ");
+     opcion = getche();
+
+     switch(opcion){
+
+     case 'a':
+         printf("\nUsted eligio sumar\n");
+         system("pause");
+         break;
+
+     case 'b':
+        printf("\nUsted eligio restar \n");
+        system("pause");
+        break;
+     case 'c':
+        printf("\nUsted eligio multiplicar\n");
+        system("pause");
+        break;
+     case 'd':
+        printf("\nUsted eligio dividir\n");
+        system("pause");
+        break;
+     case 'e':
+        printf("\nUsted eligio salir\n");
+        seguir = 'n';
+        system("pause");
+        break;
+
+     default:
+        printf("\nOpcion invalida\n\n");
+        system("pause");
+        break;
+
+     }
+
+
+
+    }while(seguir == 'n');
+
+
+
+    return 0;
+}
