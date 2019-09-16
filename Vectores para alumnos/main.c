c1525adc39a05d3ed28e46a9c8d976d39677597f
alumno <alumno@utnfra.com.ar>
2019-09-16 12:17:20 -0300

    clase 16 septiembre

--- Vectores para alumnos/main.c
+++ Vectores para alumnos/main.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TAM 5
+#define FILAS 5
+#define COLUMNAS 20
 
-void mostrarAlumnos(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam );
+void mostrarAlumnos(char matriznombres[][20], int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam );
 void mostrarAlumno(int leg, int age, int sex, int n1, int n2, float prom);
 void ordenarAlumnosLeg(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam );
 void ordenarAlumnosSexo(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam );
-void ordenarAlumnosSexLeg( int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam);
+void ordenarAlumnosSexLeg(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam);
+void mostrarNombres( char mat[][20], int filas);
+void ordenarNombres( char mat[][20], int filas);
+void ordenarAlumnosNombres(char matriznombres[][20], int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam );
 
 int main()
 {
     int legajos[TAM] = {123,45,789,321,546};
     int edades [TAM] = {20,25,18,31,29};
-    char sexos [TAM] = {'f','m','f','f','m'};
+    char sexos [TAM] = {'m','m','f','f','f'};
     int notap1 [TAM] = {5,6,7,8,9};
     int notap2 [TAM] = {5,2,7,2,9};
     float promedios [TAM] = {5,4,7,5,9};
+    char nombres[FILAS][COLUMNAS] =
+    { "Juan",
+      "Manuel",
+      "Alicia",
+      "Sofia",
+      "Claudia"
+    };
 
 
    /* for(int i = 0; i < TAM; i++)
     {
+        printf("Ingrese un nombre: ");
+        fflush(stdin);
+        gets(auxCad);
+
+        while(strlen(auxCad) >19){
+            printf("Nombre demasiado largo. Maximo 19 caracteres\nReingrese nombre: ");
+            fflush(stdin);
+            gets(auxCad);
+        }
+        strcpy(nombres[i], auxCad);
+
 
         printf("Ingrese legajo: ");
         fflush(stdin);
@@ -46,35 +70,39 @@ int main()
 */
 
 
-    mostrarAlumnos(legajos,edades,sexos,notap1,notap2,promedios,TAM);
+    mostrarAlumnos(nombres,legajos,edades,sexos,notap1,notap2,promedios,TAM);
 
     ordenarAlumnosLeg(legajos,edades,sexos,notap1,notap2,promedios,TAM);
 
-    mostrarAlumnos(legajos,edades,sexos,notap1,notap2,promedios,TAM);
+    mostrarAlumnos(nombres,legajos,edades,sexos,notap1,notap2,promedios,TAM);
 
     ordenarAlumnosSexLeg(legajos,edades,sexos,notap1,notap2,promedios,TAM);
 
-    mostrarAlumnos(legajos,edades,sexos,notap1,notap2,promedios,TAM);
+    mostrarAlumnos(nombres,legajos,edades,sexos,notap1,notap2,promedios,TAM);
+
+    ordenarAlumnosNombres(nombres,legajos,edades,sexos,notap1,notap2,promedios,TAM);
+
+    mostrarAlumnos(nombres,legajos,edades,sexos,notap1,notap2,promedios,TAM);
 
     return 0;
 }
 
-void mostrarAlumnos(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam )
+void mostrarAlumnos(char mat[][20], int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam )
 {
 
-    printf("Legajo  Edad  Sexo  Nota1  Nota2  Promedio \n\n");
+    printf("Nombre  Legajo  Edad  Sexo  Nota1  Nota2  Promedio \n\n");
     for(int i = 0; i < tam; i++)
     {
-    printf(" %d      %d      %c     %d      %d       %.2f\n",leg[i],age[i],sex[i],n1[i],n2[i],prom[i]);
+    printf(" %s  %d      %d      %c      %d       %d       %.2f\n",mat[i],leg[i],age[i],sex[i],n1[i],n2[i],prom[i]);
     }
     printf("\n\n");
 
 }
 
 
-void mostrarAlumno(int leg, int age, int sex, int n1, int n2, float prom){
+void mostrarAlumno( int leg, int age, int sex, int n1, int n2, float prom){
 
-printf(" %d      %d      %c     %d      %d       %.2f\n",leg,age,sex,n1,n2,prom);
+printf("  %d      %d      %c       %d       %d       %.2f\n",leg,age,sex,n1,n2,prom);
 
     printf("\n\n");
 
@@ -158,7 +186,7 @@ void ordenarAlumnosSexo(int leg[], int age[], char sex[], int n1[], int n2[], fl
             }
         }
 
-void ordenarAlumnosSexLeg( int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam){
+void ordenarAlumnosSexLeg(int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam){
 
     int auxInt;
     float auxFloat;
@@ -201,10 +229,79 @@ void ordenarAlumnosSexLeg( int leg[], int age[], char sex[], int n1[], int n2[],
               auxFloat = prom[i];
                prom[i] = prom[j];
                prom[j] = auxFloat;
-            }
+
+
+                }
 
             swap = 0;
         }
     }
 }
 
+void mostrarNombres( char mat[][20], int filas){
+    for(int i=0; i < filas; i++){
+        printf("%s\n", mat[i]);
+    }
+    printf("\n\n");
+}
+void ordenarNombres( char mat[][20], int filas){
+char aux[20];
+
+    for(int i=0; i < filas -1; i++){
+        for( int j = i +1; j < filas; j++){
+            if(strcmp(mat[i], mat[j]) > 0){
+
+                strcpy(aux, mat[i]);
+                strcpy(mat[i], mat[j]);
+                strcpy(mat[j], aux);
+
+            }
+        }
+    }
+}
+
+void ordenarAlumnosNombres(char mat[][20], int leg[], int age[], char sex[], int n1[], int n2[], float prom[], int tam ){
+
+    char aux[20];
+    char auxChar;
+    int auxInt;
+    float auxFloat;
+
+    for(int i=0; i < tam -1; i++){
+        for( int j = i +1; j < tam; j++){
+            if(strcmp(mat[i], mat[j]) > 0){
+
+                strcpy(aux, mat[i]);
+                strcpy(mat[i], mat[j]);
+                strcpy(mat[j], aux);
+
+                auxInt = leg[i];
+                leg[i] = leg[j];
+                leg[j] = auxInt;
+
+                auxInt = age[i];
+                age[i] = age[j];
+                age[j] = auxInt;
+
+                auxChar = sex[i];
+                 sex[i] = sex[j];
+                 sex[j] = auxChar;
+
+                auxInt = n1[i];
+                 n1[i] = n1[j];
+                 n1[j] = auxInt;
+
+                auxInt = n2[i];
+                 n2[i] = n2[j];
+                 n2[j] = auxInt;
+
+              auxFloat = prom[i];
+               prom[i] = prom[j];
+               prom[j] = auxFloat;
+
+            }
+        }
+    }
+
+
+}
---
+++ typedef struct/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    int legajo;
+    int edad;
+    int nota1;
+    int nota2;
+    char nombre[20];
+    char sexo;
+    float promedio;
+    struct{
+        int dia;
+        int mes;
+        int anio;
+    }fechadeingreso;
+}eAlumno;
+
+int main()
+{
+
+    eAlumno alumno1 = {1234,18,5,5,"Nahuel",'m',5,{17,3,2001}};
+
+    /*char aux[100];
+
+
+    printf("Ingrese nombre: ");
+    gets(aux);
+
+    while(strlen(aux)>20){
+        printf("Error. ingrese un nombre mas corto: ");
+        gets(aux);
+    }
+    strcpy(alumno1.nombre,aux);
+
+    printf("Ingrese legajo: ");
+    scanf("%d",&alumno1.legajo);
+
+    printf("Ingrese edad: ");
+    scanf("%d",&alumno1.edad);
+
+    printf("Ingrese sexo: ");
+    fflush(stdin);
+    scanf("%c",&alumno1.sexo);
+
+    printf("Ingrese nota primer parcial: ");
+    scanf("%d",&alumno1.nota1);
+
+    printf("Ingrese nota segundo parcial: ");
+    scanf("%d",&alumno1.nota2);
+
+    alumno1.promedio = (float) (alumno1.nota1 + alumno1.nota2)/2;
+
+    printf("Ingrese dia de ingreso: ");
+    scanf("%d",&alumno1.fechadeingreso.dia);
+
+    printf("Ingrese mes de ingreso: ");
+    scanf("%d",&alumno1.fechadeingreso.mes);
+
+    printf("Ingrese anio de ingreso: ");
+    scanf("%d",&alumno1.fechadeingreso.anio);
+
+    system("cls");
+*/
+
+
+    printf("Nombre: %s\nLegajo: %d\nEdad: %d\nSexo: %c\nNota primer parcial: %d\nNota segundo parcial: %d\nPromedio: %.2f\nFecha de ingreso: %02d/%02d/%d",alumno1.nombre,alumno1.legajo,alumno1.edad,alumno1.sexo,alumno1.nota1,alumno1.nota2,alumno1.promedio,alumno1.fechadeingreso.dia,alumno1.fechadeingreso.mes,alumno1.fechadeingreso.anio);
+
+    return 0;
+}
