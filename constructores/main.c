ff6e942f5fff7dcac22950fe94a226a2ab734ca9
alumno <alumno@utnfra.com.ar>
2019-10-23 12:27:22 -0300

    .

---
+++ constructores/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int dia;
+    int mes;
+    int anio;
+}eFecha;
+
+
+typedef struct{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldo;
+    eFecha fIngreso;
+}eEmpleado;
+
+int main()
+{
+
+
+
+
+    return 0;
+}
